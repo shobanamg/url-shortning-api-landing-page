02efcf20c169edacbcb6b5b5ec1edd4610a9704a
Shobana Mathiarul <mathiarulshobana@gmail.com>
2024-04-16 13:06:18 +0200

    Updated Font family in tailwind.config

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,17 @@
-import Boost from '@/components/sections/BoostLink';
+import BoostLink from '@/components/sections/BoostLink';
 import Features from '@/components/sections/Features';
 import Hero from '@/components/sections/Hero';
-import UrlShortner from '@/components/sections/UrlShortner';
+import UrlShorter from '@/components/urlShorter/UrlShorter';
 
 const Home = () => {
   return (
     <main className="overflow-hidden text-center">
       <Hero />
       <section className="bg-light-violet p-6">
-        <UrlShortner />
+        <UrlShorter />
       </section>
       <Features />
-      <Boost />
+      <BoostLink />
     </main>
   );
 };
